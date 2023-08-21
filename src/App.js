44f7434172520e6fe4cfcc9bdb7e83008e19bc70
JoyceF-9 <118493335+JoyceF-9@users.noreply.github.com>
2023-08-20 21:43:54 -0400

    Added Dictionary.js and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div classNmae="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small> Coded by Joyce</small>
+        </footer>
+      </div>
     </div>
   );
 }
